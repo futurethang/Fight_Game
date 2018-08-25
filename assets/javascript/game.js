9e212568d3922278d7d1dcce28dffa3c44034499
Kevin <kphyde@gmail.com>
2018-08-25 09:41:34 -0700

    some basic character property settings

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,14 +2,6 @@
 
 // 5 CHARACTERS AVAIALABLE, BASED ON COMMON CHARACTER OBJECT, INHERIT FUNCTIONS FROM PROTOTYPE
 
-function Person(first, last, age, eyecolor) {
-    this.firstName = first;
-    this.lastName = last;
-    this.age = age;
-    this.eyeColor = eyecolor;
-    this.name = function() {return this.firstName + " " + this.lastName;};
-}
-
 function Fighter(name, level, faction, attackPts, defencePts, imgSource) {
     
     // BASIC PROPS
@@ -21,17 +13,32 @@ function Fighter(name, level, faction, attackPts, defencePts, imgSource) {
 
     // DISPLAY PROPS
     this.imgSource = imgSource;
+    
 
     // BASIC FUNCTIONS
-    this.attack = function() {};
-    this.defend = function() {};
-    this.defeated = function() {};
-    this.won = function() {};
+    this.attack = function() {}; // includes attackPts+=10;
+    this.defend = function() {}; // defencePts-= incoming attack; 
+    this.defeated = function() {}; // actions once currentHero defencePts <= 0; Including display changes/animations
+    this.won = function() {}; // actions once currentFoe is defeated;  Including display changes/animations
 
     // optional game functions
-    this.powerUp = function() {};
-    this.badLuck = function() {};
+    this.powerUp = function() {}; // higher likelihood of power up for lower level characters
+    this.badLuck = function() {}; // higher likelihood of bad luck for lower level Imperial characters
 }
 
+// EASY MODE: initialize game characters from static sets
+let luke = new Fighter("Luke", 3, "Rebel Alliance", 25, 125, "assets/images/luke.jpg");
+let hanSolo = new Fighter("Han Solo", 3, "Rebel Alliance", 20, 150, "assets/images/han.jpg");
+let leia = new Fighter("Leia", 5, "Rebel Alliance", 30, 200, "assets/images/leia.jpg");
 
-// at game initialize 
+let darthVader = new Fighter("Darth Vader", 7, "Empire", 40, 250, "assets/images/darth.jpg");
+let stormTrooper = new Fighter("Storm Trooper", 2, "Empire", 15, 100, "assets/images/trooper.jpg");
+let badDroid = new Fighter("Imperial Droid", 2, "Empire", 10, 75, "assets/images/droid.jpg");
+
+// HARD MODE: at game initialize create random characters from the properties
+
+
+// GAMEPLAY CODE
+let currentHero; // user choice of Fighter, set to hold hero position and related game actions and display settings
+                 // Based on selection, don't fight people on your own side
+let currentFoe; // Foe, set first by choice and chosen by function later, sets related game actions and display settings
